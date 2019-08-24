69550f54815cfacf6b122d9bff431d88bb93f5b6
Billy Leitch <wlei6277@gmail.com>
2019-08-24 17:57:13 +1000

    committing final changes

--- resources/js/components/nav/NavBar.vue
+++ resources/js/components/nav/NavBar.vue
@@ -4,11 +4,6 @@
       <router-link to="/" class="navbar-item">
         <slot name="home-button-img"></slot>
       </router-link>
-      <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false">
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-      </a>
     </div>
   </nav>
 </template>
